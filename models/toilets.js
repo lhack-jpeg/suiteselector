5c65f815e3f2c9c913a90808942a06db7c66fddc
lhack-jpeg <luke-hackett@outlook.com>
2022-01-02 10:07:41 +1100

    created toilet schema

---
+++ models/toilets.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ToiletSchema = new Schema({
+    name: String,
+    code: String,
+    url: String,
+    spec: String,
+    PTrapSetout: Number,
+    STrapSetout: Number,
+    STrapMin: Number,
+    STrapMax: Number,
+});
+
+module.exports = mongoose.model('Toilet', ToiletSchema);
