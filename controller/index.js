a69c1a5ae2baaebf3f596deda02e5f583d2ab6d8
lhack-jpeg <luke-hackett@outlook.com>
2022-01-05 19:34:20 +1100

    added routing to the project

---
+++ controller/index.js
@@ -0,0 +1,40 @@
+const Toilets = require('../models/toilets');
+
+module.exports.home = (req, res) => {
+    res.render('home');
+};
+
+module.exports.search = async (req, res) => {
+    console.log(req.body);
+    const { waste, outlet } = req.body;
+    if (waste === 'STrap') {
+        const toilets = await Toilets.find({
+            $or: [
+                { STrapSetout: outlet },
+                {
+                    $and: [
+                        { STrapMin: { $lte: outlet } },
+                        { STrapMax: { $gte: outlet } },
+                    ],
+                },
+            ],
+        });
+        res.render('toilet/show', { toilets });
+    } else {
+        const toilets = await Toilets.find({ PTrapSetout: { $eq: 185 } });
+        res.render('toilet/show', { toilets });
+    }
+};
+
+module.exports.show = async (req, res) => {
+    const toilets = await Toilets.find({});
+    res.render('toilet/index', { toilets });
+};
+
+module.exports.showOne = async (req, res) => {
+    const { code } = req.params;
+    console.log(code);
+    const toilet = await Toilets.findOne({ code: code });
+    console.log(toilet);
+    res.render('toilet/showOne', { toilet });
+};
---
+++ routes/basicRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require('../utilities/wrapAsync');
+const controller = require('../controller/index');
+
+router.route('/').get(controller.home).post(wrapAsync(controller.search));
+
+router.route('/show/:code').get(wrapAsync(controller.showOne));
+
+router.route('/show').get(wrapAsync(controller.show));
+
+module.exports = router;
