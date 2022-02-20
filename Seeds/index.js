f7f674e2ef46e185a2d93a3b3fd04055c615b5d8
lhack-jpeg <luke-hackett@outlook.com>
2022-02-20 11:47:51 +1100

    update function for new toilet data

--- Seeds/index.js
+++ Seeds/index.js
@@ -27,6 +27,10 @@ const seedDB = async () => {
             STrapSetout: parseInt(toilet.STrapSetout) || null,
             STrapMin: parseInt(toilet.STrapMin) || null,
             STrapMax: parseInt(toilet.STrapMax) || null,
+            bottomInlet: toilet.bottomInlet || false,
+            backInlet: toilet.BackInlet || false,
+            waterPointHeight: parseInt(toilet.WaterPointHeight) || null,
+            waterPointOffset: parseInt(toilet.WaterPointOffset) || null,
         });
         await newToilet.save();
     }
