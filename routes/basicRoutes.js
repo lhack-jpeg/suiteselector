b5f13dd5500bbf5379c60036e1e9fbc041b637af
lhack-jpeg <luke-hackett@outlook.com>
2022-02-20 13:46:28 +1100

    fixed routes

--- routes/basicRoutes.js
+++ routes/basicRoutes.js
@@ -5,9 +5,9 @@ const controller = require('../controller/index');
 
 router.route('/').get(controller.home).post(wrapAsync(controller.search));
 
-router.route('/show/:code').get(wrapAsync(controller.showOne));
+router.route('/show/toilets/:inletType').get(wrapAsync(controller.showInlet));
 
-router.route('/show/:inletType').get(wrapAsync(controller.showInlet));
+router.route('/show/:code').get(wrapAsync(controller.showOne));
 
 router.route('/show').get(wrapAsync(controller.show));
 
