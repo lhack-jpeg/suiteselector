7e36d2e13e717fd7d78a1e6a5f05d0e9838f03f2
lhack-jpeg <luke-hackett@outlook.com>
2022-02-21 18:41:03 +1100

    fixed bug with NaN appearing on bottom inlet

--- controller/index.js
+++ controller/index.js
@@ -12,49 +12,82 @@ module.exports.home = (req, res) => {
 module.exports.search = async (req, res) => {
     console.log(req.body);
     const { waste, outlet, inletType, inletHeight, inletOffset } = req.body;
-    if (waste === 'STrap') {
-        const toiletData = await Toilets.find({
-            $and: [
-                {
-                    $or: [
-                        { STrapSetout: outlet },
-                        {
-                            $and: [
-                                { STrapMin: { $lte: outlet } },
-                                { STrapMax: { $gte: outlet } },
-                            ],
-                        },
-                    ],
-                },
-                { inletType: inletType },
-            ],
-        });
-        let toilets = toiletData;
-        for (let i = 0; i < toilets.length; i++) {
-            toilets[i].inletDistance = 0;
-            toilets[i].inletDistance = distanceCalc(
-                toilets[i].waterPointHeight,
-                toilets[i].waterPointOffset,
-                inletHeight,
-                inletOffset
-            );
+    if (inletType === 'backInlet') {
+        if (waste === 'STrap') {
+            const toiletData = await Toilets.find({
+                $and: [
+                    {
+                        $or: [
+                            { STrapSetout: outlet },
+                            {
+                                $and: [
+                                    { STrapMin: { $lte: outlet } },
+                                    { STrapMax: { $gte: outlet } },
+                                ],
+                            },
+                        ],
+                    },
+                    { inletType: inletType },
+                ],
+            });
+            let toilets = toiletData;
+            for (let i = 0; i < toilets.length; i++) {
+                toilets[i].inletDistance = 0;
+                toilets[i].inletDistance = distanceCalc(
+                    toilets[i].waterPointHeight,
+                    toilets[i].waterPointOffset,
+                    inletHeight,
+                    inletOffset
+                );
+            }
+            res.render('toilet/show', { toilets });
+        } else {
+            const toiletData = await Toilets.find({
+                $and: [
+                    { PTrapSetout: { $eq: outlet } },
+                    { inletType: inletType },
+                ],
+            });
+            let toilets = toiletData;
+            for (let i = 0; i < toilets.length; i++) {
+                toilets[i].inletDistance = 0;
+                toilets[i].inletDistance = distanceCalc(
+                    toilets[i].waterPointHeight,
+                    toilets[i].waterPointOffset,
+                    inletHeight,
+                    inletOffset
+                );
+            }
+            res.render('toilet/show', { toilets });
         }
-        res.render('toilet/show', { toilets });
     } else {
-        const toiletData = await Toilets.find({
-            $and: [{ PTrapSetout: { $eq: outlet } }, { inletType: inletType }],
-        });
-        let toilets = toiletData;
-        for (let i = 0; i < toilets.length; i++) {
-            toilets[i].inletDistance = 0;
-            toilets[i].inletDistance = distanceCalc(
-                toilets[i].waterPointHeight,
-                toilets[i].waterPointOffset,
-                inletHeight,
-                inletOffset
-            );
+        if (waste === 'STrap') {
+            const toilets = await Toilets.find({
+                $and: [
+                    {
+                        $or: [
+                            { STrapSetout: outlet },
+                            {
+                                $and: [
+                                    { STrapMin: { $lte: outlet } },
+                                    { STrapMax: { $gte: outlet } },
+                                ],
+                            },
+                        ],
+                    },
+                    { inletType: inletType },
+                ],
+            });
+            res.render('toilet/show', { toilets });
+        } else {
+            const toilets = await Toilets.find({
+                $and: [
+                    { PTrapSetout: { $eq: outlet } },
+                    { inletType: inletType },
+                ],
+            });
+            res.render('toilet/show', { toilets });
         }
-        res.render('toilet/show', { toilets });
     }
 };
 
