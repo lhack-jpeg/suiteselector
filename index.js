b4a9eb570f0c8ab1c17522c9b405f908d35f4dd0
lhack-jpeg <luke-hackett@outlook.com>
2022-12-15 14:34:58 +1100

    changed listening port

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ app.use((err, req, res, next) => {
     res.status(status).render('error', { err });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
