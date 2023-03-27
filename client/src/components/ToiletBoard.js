6bffe50d8c810888e13821f5260b852b80cf930f
Luke Hackett <luke-hackett@outlook.com>
2023-03-27 16:35:30 +1100

    change toilet board to function component

--- client/src/components/ToiletBoard.js
+++ client/src/components/ToiletBoard.js
@@ -1,43 +1,28 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
+import { useLoaderData } from "react-router-dom";
 import ToiletCard from "./ToiletCard";
 
-export default class ToiletBoard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            toilets: [],
-        };
-    }
+export function ToiletBoard() {
+    const toiletsLoaded = useLoaderData();
+    let toiletArray = toiletsLoaded.toilets.map((toilet) => (
+        <ToiletCard
+            key={toilet._id}
+            img={toilet.image}
+            name={toilet.name}
+            code={toilet.code}
+        ></ToiletCard>
+    ));
+    return (
+        <div>
+            <h4>ALL THE TOILETS</h4>
+            {toiletArray}
+        </div>
+    );
+}
 
-    componentDidMount() {
-        this.callBackEnd().then((res) =>
-            this.setState({
-                toilets: [...res.toilets],
-            }).catch((err) => console.log(err))
-        );
-    }
-
-    callBackEnd = async () => {
-        const response = await fetch("http://localhost:4000/show");
-        const body = await response.json();
-
-        return body;
-    };
-
-    render() {
-        let toilets = this.state.toilets.map((toilet) => (
-            <ToiletCard
-                key={toilet._id}
-                img={toilet.image}
-                name={toilet.name}
-                code={toilet.code}
-            ></ToiletCard>
-        ));
-        return (
-            <div>
-                <h4>ALL THE TOILETS</h4>
-                {toilets}
-            </div>
-        );
-    }
+export async function loader() {
+    const response = await fetch("http://localhost:4000/show");
+    const body = await response.json();
+    console.log({ body }, "Inside Loader");
+    return body;
 }
