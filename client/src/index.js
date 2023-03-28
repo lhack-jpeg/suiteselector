b816edfddb7813e41bb7a8532bca5fc2914be3ea
Luke Hackett <luke-hackett@outlook.com>
2023-03-28 13:53:57 +1100

    moved to route

--- client/src/index.js
+++ client/src/index.js
@@ -4,19 +4,6 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./route";
 
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                path: "show",
-                element: <ToiletBoard />,
-            },
-        ],
-    },
-]);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
--- client/src/route.js
+++ client/src/route.js
@@ -1,17 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
+import ErrorPage from "./components/ErrorPage";
 import { ToiletBoard, loader as ToiletLoader } from "./components/ToiletBoard";
 import { SingleToilet, FindOneToilet } from "./components/SingleToilet";
-import ToiletForm from "./components/ToiletSearch";
+import { ToiletForm, action as SearchAction } from "./components/ToiletSearch";
 import App from "./App";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App></App>,
+        errorElement: <ErrorPage />,
         children: [
             {
                 loader: ToiletLoader,
+                action: SearchAction,
                 path: "show",
+                element: <ToiletBoard />,
+            },
+            {
+                loader: ToiletLoader,
+                path: "show/toilets/:inletType",
                 element: <ToiletBoard />,
             },
             {
@@ -21,6 +29,7 @@ export const router = createBrowserRouter([
             },
             {
                 path: "/",
+                action: SearchAction,
                 element: <ToiletForm />,
             },
         ],
