0e1a263c1aaa674419e2a10d740e7e1f9b9cc545
Luke Hackett <luke-hackett@outlook.com>
2023-03-29 13:21:17 +1100

    move inlet board to own component

--- client/src/components/ToiletBoard.js
+++ client/src/components/ToiletBoard.js
@@ -5,24 +5,14 @@ import "./ToiletBoard.css";
 
 export function ToiletBoard() {
     const toiletsLoaded = useLoaderData();
-    let toiletArray;
-    toiletsLoaded.hasOwnProperty("toilets")
-        ? (toiletArray = toiletsLoaded.toilets.map((toilet) => (
-              <ToiletCard
-                  key={toilet._id}
-                  img={toilet.image}
-                  name={toilet.name}
-                  code={toilet.code}
-              ></ToiletCard>
-          )))
-        : (toiletArray = toiletsLoaded.map((toilet) => (
-              <ToiletCard
-                  key={toilet._id}
-                  img={toilet.image}
-                  name={toilet.name}
-                  code={toilet.code}
-              ></ToiletCard>
-          )));
+    let toiletArray = toiletsLoaded.toilets.map((toilet) => (
+        <ToiletCard
+            key={toilet._id}
+            img={toilet.image}
+            name={toilet.name}
+            code={toilet.code}
+        ></ToiletCard>
+    ));
 
     return (
         <div className="ToiletBoard">
@@ -32,19 +22,9 @@ export function ToiletBoard() {
     );
 }
 
-export async function loader({ params }) {
-    if (params.hasOwnProperty("inletType")) {
-        let inletType = params.inletType;
-        const response = await fetch(
-            `http://localhost:4000/show/toilets/${inletType}`
-        );
-        const body = await response.json();
-        console.log({ body }, "Inside Inlet Loader");
-        return body;
-    } else {
-        const response = await fetch("http://localhost:4000/show");
-        const body = await response.json();
-        console.log({ body }, "Inside Loader");
-        return body;
-    }
+export async function loader() {
+    const response = await fetch("http://localhost:4000/show");
+    const body = await response.json();
+    console.log({ body }, "Inside Loader");
+    return body;
 }
