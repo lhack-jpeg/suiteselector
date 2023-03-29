90ff09e2bd51d7144e826e9cda669b489689fd85
Luke Hackett <luke-hackett@outlook.com>
2023-03-29 13:56:58 +1100

    add styling to nav bar

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,26 +1,47 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import "./navbar.css";
 
 export default function NavBar() {
     return (
-        <div className="NavBar">
-            <div className="Logo">
+        <header className="main-header">
+            <div className="logo">
                 <Link to="/">
                     <h3>Suite Selector</h3>
                 </Link>
             </div>
-            <NavLink to="/" className="Navbar__link">
-                Home
-            </NavLink>
-            <NavLink to="/show" className="Navbar__link">
-                Show all
-            </NavLink>
-            <NavLink to="show/toilets/backInlet" className="Navbar__link">
-                Back Inlet
-            </NavLink>
-            <NavLink to="show/toilets/bottomInlet" className="Navbar__link">
-                Bottom Inlet
-            </NavLink>
-        </div>
+            <nav className="navbar">
+                <div className="main-nav">
+                    <ul className="main-nav__nav-items">
+                        <li className="main-nav__item">
+                            <NavLink to="/" className="Navbar__link">
+                                Home
+                            </NavLink>
+                        </li>
+                        <li className="main-nav__item">
+                            <NavLink to="/show" className="Navbar__link">
+                                Show all
+                            </NavLink>
+                        </li>
+                        <li className="main-nav__item">
+                            <NavLink
+                                to="show/toilets/backInlet"
+                                className="Navbar__link"
+                            >
+                                Back Inlet
+                            </NavLink>
+                        </li>
+                        <li className="main-nav__item">
+                            <NavLink
+                                to="show/toilets/bottomInlet"
+                                className="Navbar__link"
+                            >
+                                Bottom Inlet
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     );
 }
